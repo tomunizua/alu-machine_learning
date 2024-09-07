11760d06c766433f4747642bc373d4a1f99a584a
Theodora Omunizua <133643728+tomunizua@users.noreply.github.com>
2024-09-07 17:58:26 +0200

    Update 3-two.py

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -14,6 +14,8 @@ y2 = np.exp((r / t2) * x)
 plt.plot(x, y1, c="red", linestyle="dashed", label="C-14")
 plt.plot(x, y2, c="green", label="Ra-226")
 
+plt.margins(y=0)
+
 plt.xlabel("Time (years)")
 plt.ylabel("Fraction Remaining")
 plt.title("Exponential Decay of Radioactive Elements")
