f55679a31fb9e8a02741afd75201b1ff1e7b9482
tomunizua <t.omunizua@alustudent.com>
2024-09-10 17:06:23 +0000

    6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function that concatenates two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenation of two lists.
+    """
+
+    return arr1 + arr2
