ba931499c3c96ca6c90a6a445f230f250058901d
tomunizua <t.omunizua@alustudent.com>
2024-09-14 10:24:04 +0000

    10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the shape of a numpy.ndarray.
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculate the shape of a numpy.ndarray.
+    """
+    return matrix.shape
+
