4ad25f70d71a509c55d98140edea0be28fbfd94b
tomunizua <t.omunizua@alustudent.com>
2024-09-14 10:32:02 +0000

    11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function that transposes matrix using numpy.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Calculate the shape of a numpy.ndaray.
+    """
+    return matrix.T
