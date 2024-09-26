4bc262142955fd7cf93aef67600b71afe0e0dc5d
tomunizua <t.omunizua@alustudent.com>
2024-09-26 12:04:00 +0000

    9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+"""Calculates the summation"""
 
 def summation_i_squared(n):
-	if isinstance(n, int) and n > 0:
-	   return (n * (n + 1) * (2 * n + 1)) // 6
-	return None
+    """
+    Calculates the summation
+    """
+    if isinstance(n, int) and n > 0:
+        return (n * (n + 1) * (2 * n + 1)) // 6
+    return None
