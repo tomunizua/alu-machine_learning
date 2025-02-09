79925adc11b404c5373d9e9d47e42981dbd04bbf
tomunizua <your.email@example.com>
2025-02-09 15:25:45 +0200

    push

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-"""
-This module conyains a fuction that calculates the cost of a neural
-network with L2 regularization using TensorFlow/Keras.
-"""
+'''
+Implement the L2 regularization cost function
+'''
 import tensorflow as tf
 
 
-def l2_reg_cost(cost, model):
-    """
-    Calculates the cost of a neural network with L2 regularization.
-    Parameters:
-        cost (tensor): Tensor containing the cost of the network
-            without L2 regularization.
-        model (tf.keras.Model): Keras model that includes layers
-            with L2 regularization.
-    Returns:
-        tensor: A tensor containing the total cost for each layer of the
-            network, accounting for L2 regularization.
-    """
-    total_cost = cost + model.losses
-    return total_cost
+def l2_reg_cost(cost):
+    '''
+    Calculate the L2 regularization cost
+
+    Arguments:
+    cost -- cost of the neural network without regularization
+    '''
+    l2_reg_cost = tf.losses.get_regularization_losses()
+    return (cost + l2_reg_cost)
--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,36 +1,31 @@
 #!/usr/bin/env python3
-"""
-This module contains a function that creates a neural network layer with L2
-regularization using TensorFlow/Keras.
-"""
+'''
+Create a layer with L2 regularization
+using tensorflow
+'''
+
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Creates a neural network layer with L2 regularization.
-    Parameters:
-        prev (tensor): Tensor containing the output of the previous layer.
-        n (int): Number of nodes the new layer should contain.
-        activation (function): Activation function that should be used on the
-            layer.
-        lambtha (float): L2 regularization parameter.
-    Returns:
-        tensor: The output of the new layer.
+    This function creates a tensorflow layer that includes
+    L2 regularization. The layer performs matrix multiplication
+    followed by an activation function.
+
+    Arguments:
+    prev -- tensor output of the previous layer
+    n -- number of nodes in the layer to create
+    activation -- activation function that should be used on the layer
+    lambtha -- L2 regularization parameter
     """
-    # Weight initialization: He et. al
-    init_weights = tf.keras.initializers.VarianceScaling(scale=2.0,
-                                                         mode="fan_avg")
-
-    # Regularization for this layer's loss: L2 regularizer
-    l2_regularizer = tf.keras.regularizers.L2(lambtha)
-
-    # Create Dense layer with L2 regularization
-    layer = tf.keras.layers.Dense(
-        units=n,
+    l2_reg = tf.contrib.layers.l2_regularizer(lambtha)
+    weights_initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
+    layer = tf.layers.Dense(
+        n,
         activation=activation,
-        kernel_initializer=init_weights,
-        kernel_regularizer=l2_regularizer
-    )
-
-    return layer(prev)
+        name="layer",
+        kernel_initializer=weights_initializer,
+        kernel_regularizer=l2_reg)
+    return (layer(prev))
