ab462e1440ae069b1a3d97e2175de4ed4bccfa91
Theodora Omunizua <133643728+tomunizua@users.noreply.github.com>
2025-03-02 19:39:28 +0200

    fix

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,37 +1,25 @@
 #!/usr/bin/env python3
-"""
-Uses the GitHub API to print the location of a specific user,
-where user is passed as first argument of the script with full API URL
 
-ex) "./2-user_location.py https://api.github.com/users/holbertonschool"
-"""
 
+""" Return list of ships"""
 
 import requests
-from sys import argv
-from time import time
+import sys
+import time
 
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        raise TypeError(
-            "Input must have the full API URL passed in as an argument: {}{}".
-            format('ex. "./2-user_location.py',
-                   'https://api.github.com/users/holbertonschool"'))
-    try:
-        url = argv[1]
-        results = requests.get(url)
-        if results.status_code == 403:
-            reset = results.headers.get('X-Ratelimit-Reset')
-            waitTime = int(reset) - time()
-            minutes = round(waitTime / 60)
-            print('Reset in {} min'.format(minutes))
-        else:
-            results = results.json()
-            location = results.get('location')
-            if location:
-                print(location)
-            else:
-                print('Not found')
-    except Exception as err:
-        print('Not found')
+    res = requests.get(sys.argv[1])
+
+    if res.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        # get remaining rate
+
+    elif res.status_code == 404:
+        print("Not found")
+    elif res.status_code == 200:
+        res = res.json()
+        print(res['location'])
