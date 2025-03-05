df8b7a159dbd370fd20bb8c1c70d413d3cb307c8
tomunizua <your.email@example.com>
2025-03-05 16:31:52 +0200

    commit

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,18 @@
+-- Creates a stored procedure to compute average score for a user
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    -- Update the average score for the user
+    UPDATE users
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id;
+END $$
+
+DELIMITER ;
